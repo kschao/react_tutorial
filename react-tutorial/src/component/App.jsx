3f33792eee171a767ce752c090b28acd3960d190
Dustin Koy <dustin.koy@Dustins-MacBook-Pro.local>
2021-08-12 11:45:21 -0500

    coded App.jsx in tutorial videos

--- react-tutorial/src/component/App.jsx
+++ react-tutorial/src/component/App.jsx
@@ -5,13 +5,33 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "Twilight", author: "Stephanie Meyer"},
+            {title: 'New Moon', author: 'Stephanie Meyer'},
+            {title: 'Eclipse', author: 'Stephanie Meyer'}
+        ];
+        this.state = {
+            bookNumber: 1
+        };
     }
 
     render(){
         return(
             <div class= "container-fluid">
                 <TitleBar />
+                <div className="row">
+                    <div className="col-md-4">
+                        {/*button will be moved to the prev book viewed*/}
+                    </div>
+                <div className="col-md-4">
+                    {/*display book with cover here*/}
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                <div className="col-md-4">
+                    {/*button here move to next book*/}
+                    </div>
+                </div>                
             </div>
         )
     }
