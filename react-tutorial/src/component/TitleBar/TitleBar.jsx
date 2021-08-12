4259f4b9b3f2b3786504f5561ce91319232c7209
Dustin Koy <dustin.koy@Dustins-MacBook-Pro.local>
2021-08-12 11:23:11 -0500

    initalization commit on project react-tutorial

---
+++ react-tutorial/src/component/TitleBar/TitleBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TitleBar.css'
+function TitleBar(props){
+    return (
+        <div className = 'row row-space'>
+            <div className = "col-md-12" syle={{padding: 0}}>
+                <div class = "titlebar-nav">
+                    <h1>dCC Library</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default TitleBar;
